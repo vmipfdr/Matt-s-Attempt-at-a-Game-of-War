e735c2eca0a6cd120a42ed973b3cc895a2664626
vmipfdr <paynemw@yahoo.com>
2020-01-30 18:19:20 -0500

    buit deck

--- src/js/script.js
+++ src/js/script.js
@@ -11,6 +11,34 @@
 
 let deck = [];
 let suits = ['Hearts','Diamonds','Clubs','Spades'];
-let cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
-let player = ['Player1', 'Player2'];
+let faces = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+let fullDeck = []
 
+for (let i = 0; i < suits.length; i++){
+	let suit = suits[i];
+	for (let j = 0; j < faces.length; j++){
+		let face = faces[j];
+		let card = {
+			suit: suit,
+			face: face,
+		}
+		deck.push(card)
+	}
+}
+
+
+
+
+// // let cards = concat(suits + card)
+// // let player = ['Player1', 'Player2'];
+// // cards.buildCards (cards) => console.log()
+// const makePerson = (firstname, lastname) => {
+// 	return{firstname: firstname, lastname: lastname}
+// }
+// console.log(makePerson('Zakk', 'F'))
+
+
+
+// playing round 
+// var cards = ['a','b', 'c', 'd', 'e', 'f'];
+// cards.forEach(letter => console.log(letter));
