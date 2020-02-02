f1619785c9b8b296f12e872c29b781c1ef9cbe33
vmipfdr <paynemw@yahoo.com>
2020-02-02 17:22:28 -0500

    refining totalwar

--- src/js/script.js
+++ src/js/script.js
@@ -30,6 +30,16 @@ let shuffle = (array) => {
 	return array;
 }
 
+let reShuffle = (array) => {
+	for (var i = array.length - 1; i > 0; i--){
+		var j = Math.floor(Math.random()* (i + 1));
+		var temp = array[i];
+		array[i] = array [j];
+		array[j] = temp;
+	}
+	return array;
+}
+
 shuffle(deck);
 console.log('cards shuffled')
 
@@ -63,21 +73,21 @@ console.log('round complete');
 console.log(playerone, playertwo, tabledeck);
 
 let goToWar = () => {
-	// needs some refinement on amount of cards collected
 	tabledeck.push(playerone[0], playerone[1], playerone[2], playerone[3], playertwo[0], playertwo[1], playertwo[2], playertwo[3]);
-	playerone.splice(0, 3);
-	playertwo.splice(0, 3);
+	playerone.splice(0, 4);
+	playertwo.splice(0, 4);
+	console.log(playerone, playertwo, tabledeck);
 	console.log('fire for effect');
-	if (tabledeck[1].face > tabledeck[7].face) {
-			playerone.push(tabledeck[0], tabledeck[1]);
-			// account for all of the cards won
-			tabledeck.splice(0,3)
-		} else if (tabledeck[7].face > tabledeck[0].face) {
-			playertwo.push(tabledeck[0], tabledeck[1]);
-			// account for all of the cards won
-			tabledeck.splice(0,3)
-		} else if (tabledeck[0].face = tabledeck[1].face) {
-			goToWar();
+	if (tabledeck[3].face > tabledeck[7].face) {
+			console.log(tabledeck.length);
+			playerone.push(tabledeck.length);
+			tabledeck.splice(tabledeck.length);
+		} else if (tabledeck[7].face > tabledeck[3].face) {
+			console.log(tabledeck.length);
+			playertwo.push(tabledeck.length);
+			tabledeck.splice(tabledeck.length);
+		// } else if (tabledeck[3].face = tabledeck[7].face) {
+		// 	goToWar();
 		}
 }
 
@@ -91,99 +101,8 @@ let goToWar = () => {
 
 
 // goToWar(tabledeck);
+// console.log('good effects, target destroyed');
 // console.log(playerone, playertwo, tabledeck);
-
-
-
-
-
-
-// let flip = () => {
-// 	tabledeck.push(playerone[0], playertwo[0]);
-// 	playerone.splice(0,1);
-// 	playertwo.splice(0,1);
-// 	console.log('flipped');
-// }
-
-// flip(tabledeck);
-// console.log(tabledeck);
-
-// if (tabledeck[0].faces < tabledeck[1].faces); {
-// 	tabledeck.splice(0,2);
-// 	playerone.push(tabledeck[0], tabledeck[0]);
-// } else if (tabledeck[1].faces < tabledeck[0].faces); {
-// 	tabledeck.splice(0,2);
-// 	playertwo.push(tabledeck[0], tabledeck[0]);
-// }
-
-
-
-// flip(tabledeck)
-// console.log(tabledeck);
-
-// let battle = () => {
-// 	if (playerone.length !== 0) {
-// 		flip();
-// 	}
-// 	else if (playertwo.length !== 0) {
-// 		flip();
-// 	}
-// 	let flip = () => {
-// 		tabledeck.push(playerone[0], playertwo[0]);
-// 		playerone.splice(0,1);
-// 		playertwo.splice(0,1);
-// 		console.log('flipped');
-// 	if (tabledeck[0] > tabledeck[1]) {
-// 		playerone.push(tabledeck[0]faces, tabledeck[1].faces);
-// 	}
-// 	else if (tabledeck[1].faces > tabledeck[0].faces) {
-// 		playertwo.push(tabledeck[0], tabledeck[1]);
-// 	}
-// 	}
-// }
-
-// battle(tabledeck);
-// console.log(tabledeck);
-
-// while (playerone.length !== 0 || playertwo.length !==0) {
-// 	if (playerone.length < 0) {
-// 		flip();
-// 	}
-// 	else if (playertwo.length < 0) {
-// 		flip();
-// 	}
-// }
-
-
-
-
-
-
-
-//Requirements to play war
-//4. build card flipping system (unshift from each array into tableDeck)  this is where all the work is
-	//this has to have an if statement for one card higher than another
-	//another else if statement for two cards of same value (total war) (unshift 3 cards, then flip the last)
-	//build round winning collection (if statement that's used in two places?!)
-//6. build a simple announce winner if either player array is empty
-
-	// function that flips cards from player arrays into tabledeck
-		// make sure playerOne always puts their card in first
-		// if playerOne card is higher than playerTwo = pop both cards into playerOne array
-		// else if playerTwo card is higher than playerOne = pop both cards into playerTwo array
-		// else if both cards are equal than play totalWar
-			//function total war
-				// if playerOne.length < 3 can't play
-				// if playerTwo.length < 3 can't play		
-				// bring in 4 cards compare the last statement
-					//if playerOne card is higher than playerTwo = pop both cards into playerOne array
-					// else if playerTwo card is higher than playerOne = pop both cards into playerTwo array
-					// else if both cards are equal than play totalWar
-
-
-
-
-
 
 
 
